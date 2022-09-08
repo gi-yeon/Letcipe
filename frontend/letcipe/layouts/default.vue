7b87ab53486960ec1884ea4361db714901335095
jung ah <jkimlow@naver.com>
2022-09-08 16:03:31 +0900

    [FEAT] feat: create nuxt.js

---
+++ frontend/letcipe/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style>
+@font-face {
+  font-family: 'LeeSeoyun';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2202-2@1.0/LeeSeoyun.woff')
+    format('woff');
+  font-weight: bold;
+  font-style: normal;
+}
+* {
+  font-family: 'LeeSeoyun';
+}
+#app {
+  background-color: white;
+}
+</style>
