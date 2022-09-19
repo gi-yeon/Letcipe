49448e0ebb47aa7d14677dfc951fe8b6d034b305
lapaho8645 <64197428+lapaho8645@users.noreply.github.com>
2022-09-19 09:52:57 +0900

    [FEAT] feat: recipeDetail

---
+++ frontend/letcipe/pages/recipe.vue
@@ -0,0 +1,22 @@
+<template>
+  <nuxt-child></nuxt-child>
+</template>
+
+<script>
+export default {
+  name: 'RecipePage',
+}
+</script>
+
+<style scope>
+@font-face {
+  font-family: 'LeeSeoyun';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2202-2@1.0/LeeSeoyun.woff')
+    format('woff');
+  font-weight: bold;
+  font-style: normal;
+}
+* {
+  font-family: 'LeeSeoyun';
+}
+</style>
