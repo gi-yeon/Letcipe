f351c0135590b8ff2489f1c276cbd82d092e6586
민상규 <qksl7504@naver.com>
2022-09-21 17:24:05 +0900

    [FEAT] add RecipeList API

--- backend/src/main/java/com/ssafy/letcipe/domain/recipeList/RecipeListRepository.java
+++ backend/src/main/java/com/ssafy/letcipe/domain/recipeList/RecipeListRepository.java
@@ -3,6 +3,9 @@ package com.ssafy.letcipe.domain.recipeList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeListRepository extends JpaRepository<RecipeList, Long> {
+    List<RecipeList> findByNameContaining(String keyword);
 }
