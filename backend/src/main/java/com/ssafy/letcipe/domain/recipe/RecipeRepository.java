ff8c048da89ea1da6632a8d402bb2e04dba6642d
Jun <js727r@gmail.com>
2022-09-23 15:38:36 +0900

    [FIX] fix recipe search
    삭제된 레시피는 검색 결과에 나타나지 않도록 수정했습니다.

--- backend/src/main/java/com/ssafy/letcipe/domain/recipe/RecipeRepository.java
+++ backend/src/main/java/com/ssafy/letcipe/domain/recipe/RecipeRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     @Query("select distinct r from Recipe r left join fetch r.tags rp left join fetch rp.tag t " +
-            "where r.title like concat('%',:keyword,'%')" +
-            "or t.name like concat('%',:keyword,'%')")
+            "where (r.title like concat('%',:keyword,'%')" +
+            "or t.name like concat('%',:keyword,'%')) and r.isDeleted = 0")
     List<Recipe> findByKeyword(Pageable pageable, String keyword) throws SQLException;
 }
