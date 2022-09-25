d68390c611c2399a8e9ac7129561309f9106e2d8
오성준 <osj2387@naver.com>
2022-09-25 15:45:39 +0900

    [FIX] add infos for nuxt deployment

--- frontend/letcipe/nuxt.config.js
+++ frontend/letcipe/nuxt.config.js
@@ -2,6 +2,9 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   target: 'static',
+  router: {
+    base: 'S07P22A705/frontend/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'letcipe',
