00ec4884964181daa1cf1af36ea3817e3a03475e
정기연 <giyeon3145@gmail.com>
2022-09-25 17:00:14 +0900

    [FEAT] frontend dockerfile

--- frontend/letcipe/nuxt.config.js
+++ frontend/letcipe/nuxt.config.js
@@ -1,10 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  target: 'static',
-  router: {
-    base: 'S07P22A705/frontend/letcipe'
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'letcipe',
