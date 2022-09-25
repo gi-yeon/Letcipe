3d19eb9eb171f6d76797fc7604235d8068fc5246
정기연 <giyeon3145@gmail.com>
2022-09-25 16:28:24 +0900

    [FIX] fix nuxt.config.js

--- frontend/letcipe/nuxt.config.js
+++ frontend/letcipe/nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   target: 'static',
   router: {
-    base: 'S07P22A705/frontend/'
+    base: 'S07P22A705/frontend/letcipe'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
