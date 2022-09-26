73c9b53d1296056c577bb282a9290de997e03c92
Jun <js727r@gmail.com>
2022-09-26 15:27:40 +0900

    [FEAT] search recipe by ingredients
    재료로 레시피를 검색하는 API를 추가했습니다.

--- backend/src/main/java/com/ssafy/letcipe/domain/recipe/RecipeRepository.java
+++ backend/src/main/java/com/ssafy/letcipe/domain/recipe/RecipeRepository.java
@@ -18,9 +18,7 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
             "or t.name like concat('%',:keyword,'%')) and r.statusType = 0")
     List<Recipe> findByKeyword(Pageable pageable, String keyword) throws SQLException;
 
-    // select r.title, count(l.user_id)
-    // from recipe r right join recipe_like l on r.id = l.recipe_id group by r.id order by count(l.user_id) desc;
-    @Query("select r from RecipeLike l left join Recipe r on r = l.recipe group by r order by count(l) desc")
+    @Query("select l.recipe from RecipeLike l group by l order by count(l) desc")
     List<Recipe> findBestRecipes(Pageable pageable) throws SQLException;
 
     List<Recipe> findAllByUser(Pageable pageable, User user);
