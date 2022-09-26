16924f1d1ba80a9ec455a145dfb2c5a773c8d487
jung ah <jkimlow@naver.com>
2022-09-27 03:09:07 +0900

    [FEAT] feat: create store js files

---
+++ frontend/letcipe/store/cart.js
@@ -0,0 +1,128 @@
+import {
+  readCart,
+  deleteCart,
+  updateCartRecipe,
+  createCartIngredient,
+  deleteCartIngredient,
+  patchCartIngredient,
+  // startCart,
+  // createCart,
+} from '@/api/cart'
+
+export const state = () => ({
+  cart: [],
+})
+
+export const mutations = {
+  SET_CART(state, cart) {
+    state.cart = cart
+  },
+  CLEART_CART(state) {
+    state.cart = []
+  },
+}
+
+export const getters = {}
+
+export const actions = {
+  async readCart({ commit }) {
+    await readCart(
+      ({ data }) => {
+        commit('SET_CART', data.cart)
+        // console.log(data)
+        // console.log('장바구니 조회 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  //   async createCart({ commit }, recipeId) {
+  //     await createCart(
+  //         recipeId,
+  //       ({ data }) => {
+  //         // console.log(data)
+  //         // console.log('장바구니에 레시피 등록 성공!')
+  //       },
+  //       (error) => {
+  //         console.log(error)
+  //       }
+  //     )
+  //   },
+  async deleteCart({ commit }, recipeId) {
+    await deleteCart(
+      recipeId,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('장바구니 레시피 삭제 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async updateCartRecipe({ commit }, recipeAmount) {
+    await updateCartRecipe(
+      recipeAmount,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('장바구니 레시피 수량 변경 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async createCartIngredient({ commit }, cartIngredientId, operator) {
+    await createCartIngredient(
+      cartIngredientId,
+      operator,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('장바구니 추가재료 추가 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async deleteCartIngredient({ commit }, cartIngredientId, operator) {
+    await deleteCartIngredient(
+      cartIngredientId,
+      operator,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('장바구니 추가재료 삭제 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async patchCartIngredient({ commit }, recipeAmount) {
+    await patchCartIngredient(
+      recipeAmount,
+
+      ({ data }) => {
+        // console.log(data)
+        // console.log('장바구니 추가재료 수량 변경 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  //   async startCart({ commit }, calRecipeAmt) {
+  //     await startCart(
+  //         calRecipeAmt,
+
+  //       ({ data }) => {
+  //         // console.log(data)
+  //         // console.log('장바구니 실행 성공!')
+  //       },
+  //       (error) => {
+  //         console.log(error)
+  //       }
+  //     )
+  //   },
+}
---
+++ frontend/letcipe/store/recipelist.js
@@ -0,0 +1,138 @@
+import {
+  searchRecipeList,
+  createRecipeList,
+  getRecipeList,
+  updateRecipeList,
+  deleteRecipeList,
+  addRecipeListItem,
+  deleteRecipeListItem,
+  createRecipeListBookmark,
+  deleteRecipeListBookmark,
+} from '@/api/recipelist'
+
+export const state = () => ({
+  recipeListRes: [],
+})
+
+export const mutations = {
+  SET_RECIPELIST(state, recipeListRes) {
+    state.recipeListRes = recipeListRes
+  },
+}
+
+export const getters = {}
+
+export const actions = {
+  async searchRecipeList({ commit }, query, page, size) {
+    await searchRecipeList(
+      query,
+      page,
+      size,
+      ({ data }) => {
+        commit('SET_RECIPELIST', data)
+        // console.log(data)
+        // console.log('레시피리스트 검색 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async createRecipeList({ commit }, recipelist) {
+    await createRecipeList(
+      recipelist,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('레시피리스트 등록 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async getRecipeList({ commit }, recipeListId, updateRL) {
+    await getRecipeList(
+      recipeListId,
+      updateRL,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('레시피리스트 하나 검색 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async updateRecipeList({ commit }, recipeListId) {
+    await updateRecipeList(
+      recipeListId,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('레시피리스트 하나 수정 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async deleteRecipeList({ commit }, recipeListId) {
+    await deleteRecipeList(
+      recipeListId,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('레시피리스트 삭제 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async addRecipeListItem({ commit }, RLItem) {
+    await addRecipeListItem(
+      RLItem,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('레시피리스트에 레시피 추가 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async deleteRecipeListItem({ commit }, RLItem) {
+    await deleteRecipeListItem(
+      RLItem,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('레시피리스트에 레시피 삭제 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async createRecipeListBookmark({ commit }, recipeListId) {
+    await createRecipeListBookmark(
+      recipeListId,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('레시피리스트  북마크 설정 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+  async deleteRecipeListBookmark({ commit }, recipeListId) {
+    await deleteRecipeListBookmark(
+      recipeListId,
+      ({ data }) => {
+        // console.log(data)
+        // console.log('레시피리스트  북마크 해제 성공!')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  },
+}
