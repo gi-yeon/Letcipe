3f02a3e48cec9fed87c8d702ca8d1cf008db4738
김정아 <jkimlow@naver.com>
2022-09-27 11:12:46 +0900

    Update index.js

--- frontend/letcipe/config/index.js
+++ frontend/letcipe/config/index.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = 'https://j7a705.q.ssafy.io/'
-const API_BASE_URL = 'http://localhost:8888'
+const API_BASE_URL = 'https://j7a705.q.ssafy.io/'
+// const API_BASE_URL = 'http://localhost:8888'
 
 const HADOOP_BASE_URL = 'https://j7a705.q.ssafy.io/hadoop/test'
 export { API_BASE_URL, HADOOP_BASE_URL }
