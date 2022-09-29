2443969ed738f66d205bdad60ee45c6baadb0bdf
민상규 <qksl7504@naver.com>
2022-09-29 15:13:48 +0900

    [FIX] fix persistedState in plugins

--- frontend/letcipe/plugins/persistedState.js
+++ frontend/letcipe/plugins/persistedState.js
@@ -1,23 +1,16 @@
 import createPersistedState from 'vuex-persistedstate'
-import * as Cookies from 'js-cookie'
-import cookie from 'cookie'
+import Cookies from 'js-cookie'
 
 export default ({ store, req }) => {
   createPersistedState({
     paths: ['user'],
     storage: {
-      getItem: (key) => {
+      getItem: (key) =>
         // See https://nuxtjs.org/guide/plugins/#using-process-flags
-        if (process.server) {
-          const parsedCookies = cookie.parse(req.headers.cookie)
-          return parsedCookies[key]
-        } else {
-          return Cookies.get(key)
-        }
-      },
+        Cookies.get(key),
       // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
       setItem: (key, value) =>
-        Cookies.set(key, value, { expires: 365, secure: false }),
+        Cookies.set(key, value, { expires: 3, secure: false }),
       removeItem: (key) => Cookies.remove(key),
     },
   })(store)
