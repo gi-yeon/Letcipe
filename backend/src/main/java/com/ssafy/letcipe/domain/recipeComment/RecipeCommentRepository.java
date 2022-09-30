e9876845b28472ebc12b4cecb975849e56bf34d9
lapaho8645 <64197428+lapaho8645@users.noreply.github.com>
2022-09-30 18:17:11 +0900

    [FIX] cartAndRecipe

--- backend/src/main/java/com/ssafy/letcipe/domain/recipeComment/RecipeCommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.letcipe.domain.recipeComment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeCommentRepository extends JpaRepository<RecipeComment, Long> {
-}
